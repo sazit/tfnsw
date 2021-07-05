7a28abfe26fa5fcf404b009edbc8070e0d2e0807
Sajit Gurubacharya <sazit@Sajits-MacBook-Pro.local>
2021-07-06 00:38:49 +1000

    added plotly mapping on openstreetmap overlay

--- tfnsw.py
+++ tfnsw.py
@@ -17,7 +17,7 @@ source data: https://opendata.transport.nsw.gov.au/dataset/timetables-complete-g
 
 def get_gtfs_data():
     URL = "https://api.transport.nsw.gov.au/v1/publictransport/timetables/complete/gtfs"
-    API_KEY = 'XXXXXXXXXX'  # masked
+    API_KEY = 'BJphVVSCi3hZFTzt1imtH4rTpWK0an6EBaZk'  # masked
     headers = dict(Authorization='apikey ' + API_KEY)
     print("Requesting new GTFS data...")
     response = requests.get(URL, headers=headers)
@@ -30,7 +30,8 @@ def get_gtfs_data():
 
     if response:
         print("Request successful, downloading and extracting file")
-        open('../gtfs_static.zip', 'wb').write(response.content) # double check directory
+        # double check directory
+        open('../gtfs_static.zip', 'wb').write(response.content)
         zip = zipfile.ZipFile('../gtfs_static.zip')
         zip.extractall('../')
         return True
@@ -58,20 +59,27 @@ expected files:
 # opening stops.txt
 data = pd.read_csv(r'../stops.txt')
 stops_df = pd.DataFrame(data)
-print(stops_df.head())
 
+# stops_df = stops_df[(stops_df['parent_station'] == 'Parramatta')] # Alteryx filter tool
+stops_df = stops_df[(stops_df['stop_lon'] < 151.0321) & (stops_df['stop_lon'] > 150.9851) & (
+    stops_df['stop_lat'] < -33.7999) & (stops_df['stop_lat'] > -33.8376)]  # Alteryx filter tool
 stops_df_sample = stops_df.sample(n=200)
-bounding_box = (stops_df_sample.stop_lon.min(),   stops_df_sample.stop_lat.max(),      
-         stops_df_sample.stop_lat.min(), stops_df_sample.stop_lon.max())
+print(stops_df_sample)
+# bounding_box = (stops_df_sample.stop_lon.min(),   stops_df_sample.stop_lat.max(),
+#          stops_df_sample.stop_lat.min(), stops_df_sample.stop_lon.max())
+# greater Sydney area excl west of blacktown
+bounding_box = (150.8, -33.6, -34.1, 151.38)
 
 print(bounding_box)
 
+
 # using plotly, but can try to implement OSM API to get bounded area map and overlay points
-fig = px.scatter_geo(stops_df_sample,lat='stop_lat',lon='stop_lon', hover_name="stop_name")
-fig.update_layout(title = 'Sample Stops Map - TfNSW', title_x=0.5)
+# fig = px.line_mapbox(stops_df_sample,lat='stop_lat',lon='stop_lon', hover_name="stop_name")
+fig = px.scatter_mapbox(stops_df_sample, lat='stop_lat',
+                        lon='stop_lon', hover_name="stop_name")
+fig.update_layout(title='Sample Stops Map - TfNSW', title_x=0.5)
+fig.update_layout(mapbox_style="open-street-map")
 fig.show()
-
-
 
 
 ## ------- extra code ----- ##
